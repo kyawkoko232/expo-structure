d7f65f915d78e21970209bf5490f086e57b18b18
kyawkoko232 <kyawkoko9m@gmail.com>
2024-10-01 03:32:12 +0630

    i18n fixed

--- i18n/index.ts
+++ i18n/index.ts
@@ -3,23 +3,13 @@ import { initReactI18next } from "react-i18next";
 import * as Localization from "expo-localization";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import translationEn from "./locales/en-US/translation.json";
-import translationPt from "./locales/pt-BR/translation.json";
-import translationRu from "./locales/ru-RU/translation.json";
 import translationZh from "./locales/zh-CN/translation.json";
-import translationEs from "./locales/es-ES/translation.json";
-import translationIt from "./locales/it-IT/translation.json";
-import translationHi from "./locales/hi-IN/translation.json";
 import translationMM from "./locales/mm-MM/translation.json";
 
 // Configuração das traduções
 const resources = {
-  "pt-BR": { translation: translationPt },
   "en-US": { translation: translationEn },
-  "es-ES": { translation: translationEs },
-  "ru-RU": { translation: translationRu },
   "zh-CN": { translation: translationZh },
-  "it-IT": { translation: translationIt },
-  "hi-IN": { translation: translationHi },
   "mm-MM": { translation: translationMM },
 };
 
